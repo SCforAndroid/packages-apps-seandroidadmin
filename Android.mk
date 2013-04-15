fd07a1be3caf1a373bd3f2354f0122135bccda5f
Stephen Smalley <sds@tycho.nsa.gov>
2013-04-15 09:28:21 -0400

    Rename and update strings for SEAdmin.
    Eliminate "SEAndroid" or "SE Android" from package name and strings.
    
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := SEAndroidAdmin
+LOCAL_PACKAGE_NAME := SEAdmin
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
