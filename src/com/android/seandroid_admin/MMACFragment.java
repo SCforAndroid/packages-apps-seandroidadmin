35a9afdd85020bf51869c188f118e919324f9b96
Simon Busch <morphis@gravedo.de>
2013-08-06 10:33:26 +0000

    Import modifications from Intent MAC layyer from intent_mac branch

--- src/com/android/seandroid_admin/MMACFragment.java
+++ src/com/android/seandroid_admin/MMACFragment.java
@@ -23,15 +23,24 @@ public class MMACFragment extends SEAndroidAdminFragment
     private static final String KEY_MMAC_ENFORCING = "key_mmac_enforcing";
     private static final String KEY_MMAC_RELOAD = "key_mmac_reload";
     private static final String KEY_MMAC_RESTORE = "key_mmac_restore";
+    private static final String KEY_INTENT_RELOAD = "key_intent_reload";
+    private static final String KEY_INTENT_RESTORE = "key_intent_restore";
+    private static final String KEY_TYPES_RELOAD = "key_types_reload";
+    private static final String KEY_TYPES_RESTORE = "key_types_restore";
 
     private static final String MMAC_ENFORCE_PROPERTY = "persist.mmac.enforce";
     private static final String MMAC_POLICY_FILE = "mac_permissions.xml";
+    private static final String INTENT_POLICY_FILE = "intent_mac.xml";
+    private static final String TYPES_POLICY_FILE = "mmac_types.xml";
 
     private CheckBoxPreference mMMACenforceCheckbox;
-    private Preference mMMACreload;
-    private Preference mMMACrestore;
+    private Preference mMMACreload, mMMACrestore;
+    private Preference mIntentReload, mIntentRestore;
+    private Preference mTypesReload, mTypesRestore;
 
     private File mMMACpolicyFile = null;
+    private File mIntentPolicyFile = null;
+    private File mTypesPolicyFile = null;
 
     private String mMMACenforceCheckboxSummaryChecked;
     private String mMMACenforceCheckboxSummaryUnchecked;
@@ -59,6 +68,8 @@ public class MMACFragment extends SEAndroidAdminFragment
             File extFileDir = mActivity.getExternalFilesDir(null);
             if (extFileDir != null) {
                 mMMACpolicyFile = new File(extFileDir, MMAC_POLICY_FILE);
+                mIntentPolicyFile = new File(extFileDir, INTENT_POLICY_FILE);
+                mTypesPolicyFile = new File(extFileDir, TYPES_POLICY_FILE);
             }
 
             mMMACenforceCheckbox =
@@ -104,6 +115,82 @@ public class MMACFragment extends SEAndroidAdminFragment
                         Toast.makeText(mActivity, "Unable to remove custom policy", Toast.LENGTH_SHORT).show();
                     } else {
                         Toast.makeText(mActivity, "Success", Toast.LENGTH_SHORT).show();
+                    }
+                    return false;
+                }
+            });
+            
+            mIntentReload =
+                    getPreferenceScreen().findPreference(KEY_INTENT_RELOAD);
+            mIntentReload.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Log.v(TAG, "Reload of Intent policy requested");
+                    try {
+                        byte[] policy = Files.toByteArray(mIntentPolicyFile);
+                        if (!mAdmin.mDPM.setCustomPolicyFile(mAdmin.mDeviceAdmin,
+                                DevicePolicyManager.INTENT_POLICY_FILE, policy)) {
+                            Toast.makeText(mActivity, "Unable to set policy", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(mActivity, "Success", Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (IOException ioex) {
+                        Log.e(TAG, "Exception ocurred", ioex);
+                        Toast.makeText(mActivity, ioex.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                    return false;
+                }
+            });
+
+            mIntentRestore =
+                    getPreferenceScreen().findPreference(KEY_INTENT_RESTORE);
+            mIntentRestore.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Log.v(TAG, "Delete custom Intent policy requested");
+                    if (!mAdmin.mDPM.setCustomPolicyFile(mAdmin.mDeviceAdmin,
+                            DevicePolicyManager.INTENT_POLICY_FILE, null)) {
+                        Toast.makeText(mActivity, "Unable to remove custom policy", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(mActivity, "Success", Toast.LENGTH_SHORT).show();
+                    }
+                    return false;
+                }
+            });
+
+            mTypesRestore =
+                    getPreferenceScreen().findPreference(KEY_TYPES_RESTORE);
+            mTypesRestore.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Log.v(TAG, "Delete custom MMAC types requested");
+                    if (!mAdmin.mDPM.setCustomPolicyFile(mAdmin.mDeviceAdmin,
+                            DevicePolicyManager.MMAC_TYPES_FILE, null)) {
+                        Toast.makeText(mActivity, "Unable to remove custom policy", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(mActivity, "Success", Toast.LENGTH_SHORT).show();
+                    }
+                    return false;
+                }
+            });
+
+            mTypesReload =
+                    getPreferenceScreen().findPreference(KEY_TYPES_RELOAD);
+            mTypesReload.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Log.v(TAG, "Reload of MMAC types requested");
+                    try {
+                        byte[] policy = Files.toByteArray(mTypesPolicyFile);
+                        if (!mAdmin.mDPM.setCustomPolicyFile(mAdmin.mDeviceAdmin,
+                                DevicePolicyManager.MMAC_TYPES_FILE, policy)) {
+                            Toast.makeText(mActivity, "Unable to set policy", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(mActivity, "Success", Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (IOException ioex) {
+                        Log.e(TAG, "Exception ocurred", ioex);
+                        Toast.makeText(mActivity, ioex.toString(), Toast.LENGTH_SHORT).show();
                     }
                     return false;
                 }
@@ -157,6 +244,23 @@ public class MMACFragment extends SEAndroidAdminFragment
             }
 
             mMMACrestore.setEnabled(true);
+            
+            mIntentReload.setEnabled(true);
+            if (mMMACpolicyFile != null) {
+                mIntentReload.setSummary(mIntentPolicyFile.getPath());
+            } else {
+                mIntentReload.setSummary(R.string.ext_storage_unavail);
+            }
+
+            mIntentRestore.setEnabled(true);
+
+            mTypesReload.setEnabled(true);
+            if (mTypesPolicyFile != null) {
+                mTypesReload.setSummary(mTypesPolicyFile.getPath());
+            } else {
+                mTypesReload.setSummary(R.string.ext_storage_unavail);
+            }
+            mTypesRestore.setEnabled(true);
         }
     }
 
